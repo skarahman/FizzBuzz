7f10c5ac66284c6993439c65738101c767bc2d0c
Linkon <skarahman@gmail.com>
2012-11-26 18:37:08 +0600

    If a number is divisible by 5 print "Buzz" instead

--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -18,8 +18,11 @@ namespace FizzBuzz
             for (int i = 1; i <= 100; i++)
             {
                 bool fizz = i % 3 == 0;
+                bool buzz = i % 5 == 0;
                 if (fizz)
                     Console.WriteLine("Fizz");
+                else if(buzz)
+                    Console.WriteLine("Buzz");
                 else
                 {
                     Console.WriteLine(i);
